4b41b6df36673677fcd28889354cd4cc968c534b
raj-p26 <rajpeshavariya26@gmail.com>
2024-07-08 01:20:28 +0530

    Added simple middleware(hook) with jsonwebtoken

---
+++ src/hooks.server.ts
@@ -0,0 +1,44 @@
+import { json, type Handle } from "@sveltejs/kit";
+import { verify, sign } from 'jsonwebtoken';
+
+export const handle: Handle = async ({ event, resolve }) => {
+    let { url, request } = event;
+    let { pathname } = url;
+
+    if (pathname == "/api/products" && request.method == "POST") {
+        let { success, next } = await authenticate_products_api(request);
+        if (!success) return next;
+
+    }
+
+    return resolve(event);
+}
+
+async function authenticate_products_api(request: Request) {
+    const auth_token = request.headers.get('authorization');
+
+    if (auth_token == null)
+        return {
+            success: false,
+            next: json({
+                status: "failed",
+                message: "Unauthorized",
+            }, {
+                status: 401,
+            })
+        };
+
+    try {
+        verify(auth_token, process.env.JWT_SECRET as string);
+    } catch (err: any) {
+        return {
+            success: false,
+            next: json({
+                status: 'failed',
+                message: err.message,
+            })
+        };
+    }
+
+    return { success: true, next: new Response() };
+}
